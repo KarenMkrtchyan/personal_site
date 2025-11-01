0584849a2b4b00adc7ca605a71c29da3c843731c
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-11-01 15:39:17 -0700

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <div>
+      <section className="relative h-[200px] md:h-[300px]">
+        <Image
+          src="/banner.jpg"
+          alt="Beautiful banner"
+          fill
+          priority
+          className="object-cover"
+        />
+      </section>
+      <div>
+        <div>
+          1 serving of Computer Engineering and Science @ USC, with a side of
+          Cybersecurity Cooking at high heat for 2 more years (May 27
+          graduation)
+          <br />I love building things that blend hardware and software that
+          make people’s lives better. I’m interested in mathematics, AI/ML,
+          distributed systems, big code, robotics, investing (crypto & kalshi),
+          and risking my life for bursts of adrenaline
+        </div>
+        <section className="relative w-[50px] md:w-[75px] h-[50px] md:h-[75px]">
+          <Image
+            src="/me.jpg"
+            alt="Beautiful man"
+            fill
+            priority
+            className="object-cover"
+          />
+        </section>
+      </div>
+      <div>Content list</div>
+      <div>Current</div>
+      <div>Past</div>
+    </div>
+  );
 }
