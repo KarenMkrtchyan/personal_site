950200390de71bb9798ebf052f70e069ead39db7
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-11-02 13:40:47 -0800

    banner and cented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="flex justify-center items-center">
-      <body>{children}</body>
+      <head>
+        <title>hi i'm ka'ren</title>
+      </head>
+      <body className="bg-base-bg flex flex-col items-center">
+        <div className="relative w-screen h-[300px]">
+          <Image
+            src="/banner.jpg"
+            alt="Banner"
+            fill
+            priority
+            style={{ objectFit: "cover" }}
+          />
+
+          {/* Gradient Overlay */}
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: "60%",
+              background:
+                "linear-gradient(to bottom, rgba(25,25,25,0) 0%, #191919 100%)",
+            }}
+          />
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
